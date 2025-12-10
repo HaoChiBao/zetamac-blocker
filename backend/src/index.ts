b3a308be13684131989d2f1c7f83820f31110f6a
James Yang <jamesyang663@gmail.com>
2025-12-10 00:07:18 -0500

    a

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -49,6 +49,6 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(Number(PORT), '127.0.0.1', () => {
-    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+app.listen(Number(PORT), '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
